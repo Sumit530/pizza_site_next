3ab80a10b8269a235306486873c3df036442136b
sumit530 <sp43216@gmail.com>
2023-01-13 17:18:13 +0530

    commit

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 const express = require("express")
 const app = express()
-const http = require("http")
+const https = require("https")
 const moment = require("moment")
 const { CLIENT_RENEG_LIMIT } = require("tls")
 const router = require("./routes/routes")
 const multer = require("multer")
+const fs =require("fs")
+const path = require("path")
 const upload = multer()
 app.use(upload.none()) 
 const bodyparser= require("body-parser")
-const server = http.createServer(app)
+const server = https.createServer(
+   {
+     key:fs.readFileSync(path.join(__dirname,"cert",'key.pem')),
+  cert:fs.readFileSync(path.join(__dirname,"cert",'cert.pem')),
+},
+app)
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+app.use("/peerjs", peerServer);
+app.use(express.static("public"));
 require("./db/connection")
 app.use('/api',router)
 //app.use(con)
 
-
-app.listen(8000,()=>{
+app.get("/",(req,res)=>{
+    res.send("hey")
+})
+server.listen(8000,()=>{
 console.log("server is running on 8000");
 })  
